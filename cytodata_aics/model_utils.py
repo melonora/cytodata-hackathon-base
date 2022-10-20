b9fb4082397995886fbd185b87f052839014ff75
Tra Ngo <tra.huongngo@gmail.com>
2022-10-20 18:24:51 +0000

    commit all changes to model_utils.py

--- cytodata_aics/model_utils.py
+++ cytodata_aics/model_utils.py
@@ -19,19 +19,35 @@ def save_predictions_classifier(preds, output_dir):
     )
     
 def save_predictions_cell_stage(preds, output_dir):
+#     """
+#     TODO: DOESNT WORK PROPERLY! get 5 class predictions, need max from probability outputs
+#     """
+#     records = []
+#     for pred in preds:
+#         record = dict()
+#         for col in ["id", "y", "yhat"]:
+#             record[col] = pred[col].argmax()
+#         record["loss"] = [pred["loss"].item()] * len(pred["id"])
+#         records.append(pd.DataFrame(record))
+    
+#     pd.concat(records).reset_index().drop(columns="index").to_csv(
+#         Path(output_dir) / "model_predictions.csv", index_label=False
+#     )
     """
-    TODO: DOESNT WORK PROPERLY! get 5 class predictions, need max from probability outputs
+    TODO: make this better? maybe use vol predictor code?
+    TODO: drop unnecessary index
     """
     records = []
     for pred in preds:
         record = dict()
-        for col in ["id", "y", "yhat"]:
-            record[col] = pred[col].argmax()
+        for col in ["id", "y"]:
+            record[col] = pred[col].squeeze().numpy()
+        for col in ["yhat"]:
+            record[col] = np.argmax(pred[col].numpy(), 1)
         record["loss"] = [pred["loss"].item()] * len(pred["id"])
         records.append(pd.DataFrame(record))
-    
     pd.concat(records).reset_index().drop(columns="index").to_csv(
-        Path(output_dir) / "model_predictions.csv", index_label=False
+        Path(output_dir) / "model_predictions_multiclass.csv", index_label=False
     )
     
 def save_predictions_classifier_multiclass(preds, output_dir):
