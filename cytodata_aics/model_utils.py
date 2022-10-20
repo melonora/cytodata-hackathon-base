f5fc01b99f41ccd6b4db7de9f01d7933fadd8c70
Tra Ngo <tra.huongngo@gmail.com>
2022-10-20 06:01:18 +0000

    changes to multi brightfield and model_utils.py correct file pointer bug

--- cytodata_aics/model_utils.py
+++ cytodata_aics/model_utils.py
@@ -17,10 +17,10 @@ def save_predictions_classifier(preds, output_dir):
     pd.concat(records).reset_index().drop(columns="index").to_csv(
         Path(output_dir) / "model_predictions.csv", index_label=False
     )
-
+    
 def save_predictions_cell_stage(preds, output_dir):
     """
-    TODO: get 5 class predictions, need max from probability outputs
+    TODO: DOESNT WORK PROPERLY! get 5 class predictions, need max from probability outputs
     """
     records = []
     for pred in preds:
@@ -34,13 +34,20 @@ def save_predictions_cell_stage(preds, output_dir):
         Path(output_dir) / "model_predictions.csv", index_label=False
     )
     
+def save_predictions_classifier_multiclass(preds, output_dir):
     """
+    TODO: make this better? maybe use vol predictor code?
+    TODO: drop unnecessary index
+    """
+    records = []
     for pred in preds:
         record = dict()
         for col in ["id", "y"]:
             record[col] = pred[col].squeeze().numpy()
         for col in ["yhat"]:
-            record[col] = (pred[col].squeeze().numpy().argmax()
+            record[col] = np.argmax(pred[col].numpy(), 1)
         record["loss"] = [pred["loss"].item()] * len(pred["id"])
         records.append(pd.DataFrame(record))
-    """
+    pd.concat(records).reset_index().drop(columns="index").to_csv(
+        Path(output_dir) / "model_predictions_multiclass.csv", index_label=False
+    )
